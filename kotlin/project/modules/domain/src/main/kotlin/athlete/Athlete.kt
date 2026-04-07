a6b30fcb350c192e8cb5799db2a5d4e0f03814e7
Tomás Pereira <A49486@alunos.isel.pt>
2026-04-07 14:03:11 +0100

    athlete update

--- kotlin/project/modules/domain/src/main/kotlin/athlete/Athlete.kt
+++ kotlin/project/modules/domain/src/main/kotlin/athlete/Athlete.kt
@@ -8,7 +8,13 @@ data class Athlete(
     val memberId: Long, // FK → Member (todo atleta é sócio)
     val nationality: String,
     val birthplace: String,
-    // dado que todo atleta é socio precisamos de data de nascimento, email, telemovel aqui?
+    val birthdate: LocalDate,
+    val email: String,
+    val phone: String,
+    val postalCode: String,
+    val adress: String,
+    val city: String,
+    val state: String,
     val niss: String, // Número de Identificação da Segurança Social
     val nif: String, // Número de Identificação Fiscal
     val numeroUtente: String, // Número de Utente da Saúde
@@ -21,4 +27,8 @@ data class Athlete(
     val season: String?, // Época desportiva (ex: "2025/2026")
     val teamCategory: TeamCategory, // Escalão em que joga
     val active: Boolean = true, // Se ainda é atleta ativo do clube
+    val privacyAccepted: Boolean = false,
+    val comsAccepted: Boolean = false,
+    val schoolCertificationAccepted: Boolean = false,
+    val guardians: List<Guardian> = emptyList(),
 )
--- kotlin/project/modules/domain/src/main/kotlin/athlete/Guardian.kt
+++ kotlin/project/modules/domain/src/main/kotlin/athlete/Guardian.kt
@@ -5,10 +5,11 @@ data class Guardian(
     val athleteId: Long, // Aponta para o Atleta (Menor)
     val memberId: Long?, // Caso o encarregado de educação seja socio
     val name: String,
+    val role: GuardianRole,
     val kinship: String, // Grau de Parentesco (Pai, Mãe, etc.)
     val email: String,
     val phone: String,
     val work: String,
-    val isGuardian: Boolean,
+    val isPrimary: Boolean = false,
     val hasFamilyInClub: Boolean,
 )
