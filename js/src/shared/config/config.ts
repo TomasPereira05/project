532d1f34a3d97389e66ac47bd58a373a3ae19385
Tomás Rocha <A49481@alunos.isel.pt>
2026-04-29 19:34:51 +0100

    User logged in icon & optimizations

--- js/src/shared/config/config.ts
+++ js/src/shared/config/config.ts
@@ -1,3 +1,3 @@
 export const BASE_URL = "/api";
-export const LOGO_SRC = '../images/logo_GDUE.png';
-export const HERO_IMG_SRC = '../images/hero_Background.jpg';
+export const LOGO_SRC = '/images/logo_GDUE.png';
+export const HERO_IMG_SRC = '/images/hero_Background.jpg';
